0aec101474a0b640fa5373ce44ef40e2fa84ef92
RezaRiyaldi <52364173+RezaRiyaldi@users.noreply.github.com>
2020-11-08 15:54:32 +0700

    Update program1.py

--- program1.py
+++ program1.py
@@ -2,6 +2,10 @@ modal = 100000000
 laba = [modal * 0, modal* 0, modal * 1/100, modal * 1/100, modal * 5/100, modal * 5/100, modal * 5/100, modal * 3/100]
 bulan = 0
 sum = 0
+print(39*"=")
+print(5*"=","Menghitung Total Keuntungan", 5*"=")
+print(39*"=")
+print(f"Modal awal pengusaha {modal}")
 for i in laba:
     sum = sum + i
     bulan += 1
